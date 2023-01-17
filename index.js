78bd8fb3a2d5cb99a29e27f294ba1f715215a99d
jmgarcia <jmgarcia@esatvalencia.lan>
2023-01-17 19:16:06 +0100

    initial styles for form

---
+++ index.js
@@ -0,0 +1,17 @@
+const testData = [
+    {
+        id: 10,
+        description: 'Hacer la compra',
+        isCompleted: false,
+    },
+    {
+        id: 2,
+        description: 'Hacer la cama',
+        isCompleted: false,
+    },
+    {
+        id: 28,
+        description: 'Hacer el proyecto about me',
+        isCompleted: true,
+    }
+]
